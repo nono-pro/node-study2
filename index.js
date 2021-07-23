0630f41371a59931e9fd9dc53a4b0f30e45d4483
nono-pro <87250590+nono-pro@users.noreply.github.com>
2021-07-23 16:01:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('YES?')
+  response.send('YES!')
 })
 
 app.listen(app.get('port'), function() {
